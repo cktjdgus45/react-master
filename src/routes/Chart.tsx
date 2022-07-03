76117e431085a5c637e1341e6f301bb0ef30cf42
pooh <cktjdgus45@naver.com>
2022-07-03 17:38:42 +0900

    add coin end price chart

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,17 +1,95 @@
 import React from 'react';
 import { useQuery } from 'react-query';
 import { fetchCoinHistory } from '../api';
+import ApexChart from "react-apexcharts";
 
 interface ChartProps {
     coinId: string;
 }
 
+interface IHistroical {
+    time_open: number
+    time_close: number
+    open: string
+    high: string
+    low: string
+    close: number
+    volume: string
+    market_cap: number
+}
+
 const Chart = ({ coinId }: ChartProps) => {
     console.log(coinId)
-    const { isLoading, data } = useQuery(["ohlcv", coinId], () => fetchCoinHistory(coinId))
+    const { isLoading, data } = useQuery<IHistroical[]>(["ohlcv", coinId], () => fetchCoinHistory(coinId))
     return (
         <>
-            <h1>chart</h1>
+            {isLoading ? "Loading Chart..." : (
+                <ApexChart
+                    type='line'
+                    options={
+                        {
+                            stroke: {
+                                curve: "smooth",
+                                width: 4
+                            },
+                            chart: {
+                                id: 'coin-price',
+                                toolbar: {
+                                    show: false,
+                                },
+                                background: "transparent"
+                            },
+                            grid: {
+                                show: false
+                            },
+                            fill: {
+                                type: 'gradient',
+                                gradient: {
+                                    gradientToColors: ['#FFFFFF'],
+                                    stops: [0, 100],
+                                },
+                            },
+                            colors: ['#FF0063'],
+                            theme: {
+                                mode: 'dark',
+                            },
+                            yaxis: {
+                                labels: {
+                                    show: false
+                                }
+                            },
+                            xaxis: {
+                                type: 'datetime',
+                                labels: {
+                                    show: false,
+                                    datetimeFormatter: { month: "mmm yy" }
+                                },
+                                axisTicks: {
+                                    show: false
+                                },
+                                axisBorder: {
+                                    show: false
+                                },
+                                categories: data?.map(date => date.time_close)
+                            },
+                            tooltip: {
+                                y: {
+                                    formatter: (value) => `${value.toFixed(0)}K`
+                                }
+                            }
+                        }
+                    }
+                    width="500"
+                    height="300"
+                    series={
+                        [
+                            {
+                                name: '종가',
+                                data: data?.map(price => price.close) as number[],
+                            },
+                        ]
+                    }
+                />)}
         </>
     )
 }
