d10a39223fa5f711c58f0b83a6076942dd1d5ca4
pooh <cktjdgus45@naver.com>
2022-07-27 17:59:56 +0900

    practice framer-motion:animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,93 +1,7 @@
-import { createGlobalStyle, } from 'styled-components';
 import styled, { } from 'styled-components';
+import { motion } from "framer-motion"
 
 
-const GlobalStyle = createGlobalStyle`
-  @import url(//spoqa.github.io/spoqa-han-sans/css/SpoqaHanSansNeo.css);
-  * { font-family: 'Spoqa Han Sans Neo', 'sans-serif'; }
-html, body, div, span, applet, object, iframe,
-h1, h2, h3, h4, h5, h6, p, blockquote, pre,
-a, abbr, acronym, address, big, cite, code,
-del, dfn, em, img, ins, kbd, q, s, samp,
-small, strike, strong, sub, sup, tt, var,
-b, u, i, center,
-dl, dt, dd, menu, ol, ul, li,
-fieldset, form, label, legend,
-table, caption, tbody, tfoot, thead, tr, th, td,
-article, aside, canvas, details, embed,
-figure, figcaption, footer, header, hgroup,
-main, menu, nav, output, ruby, section, summary,
-time, mark, audio, video {
-  margin: 0;
-  padding: 0;
-  border: 0;
-  font-size: 100%;
-  font: inherit;
-  vertical-align: baseline;
-}
-
-/* HTML5 display-role reset for older browsers */
-article, aside, details, figcaption, figure,
-footer, header, hgroup, main, menu, nav, section {
-  display: block;
-}
-/* HTML5 hidden-attribute fix for newer browsers */
-*[hidden] {
-    display: none;
-}
-html{
-	&::-webkit-scrollbar{
-        width: 0.0000000001px;
-	} 
-	&::-webkit-scrollbar-thumb{
-        opacity:0;
-        pointer-events: none;
-        z-index: -99999;
-	}
-	&::-webkit-scrollbar-track{
-        opacity:0;
-        pointer-events:none;
-        z-index : -99999;
-	} 
-}
-menu, ol, ul {
-  list-style: none;
-}
-blockquote, q {
-  quotes: none;
-}
-blockquote:before, blockquote:after,
-q:before, q:after {
-  content: '';
-  content: none;
-}
-table {
-  border-collapse: collapse;
-  border-spacing: 0;
-}
-* {
-  box-sizing: border-box;
-}
-body {
-  font-weight: 300;
-  font-family: 'Source Sans Pro', sans-serif;
-  line-height: 1.2;
-  background:linear-gradient(135deg,#e09,#d0e);
-}
-a {
-  text-decoration:none;
-  color:inherit;
-}
-input{
-  border: none;
-  outline: none;
-}
-
-button{
-  cursor: pointer;
-}
-
-`;
 
 
 const Wrapper = styled.div`
@@ -98,7 +12,7 @@ const Wrapper = styled.div`
   align-items: center;
 `;
 
-const Box = styled.div`
+const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
   background-color: white;
@@ -111,12 +25,16 @@ const Box = styled.div`
 
 function App() {
 
-	return (
-		<Wrapper>
-			<GlobalStyle />
-			<Box />
-		</Wrapper>
-	)
+  return (
+    <Wrapper>
+      <Box
+        initial={{ scale: 0 }}
+        animate={{ scale: 1, rotateZ: 360 }}
+        transition={{ type: 'spring', bounce: 0.3 }}
+      />
+
+    </Wrapper>
+  )
 
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import { createGlobalStyle, } from 'styled-components';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { RecoilRoot } from 'recoil';
@@ -6,8 +7,96 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
+const GlobalStyle = createGlobalStyle`
+  @import url(//spoqa.github.io/spoqa-han-sans/css/SpoqaHanSansNeo.css);
+  * { font-family: 'Spoqa Han Sans Neo', 'sans-serif'; }
+html, body, div, span, applet, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+a, abbr, acronym, address, big, cite, code,
+del, dfn, em, img, ins, kbd, q, s, samp,
+small, strike, strong, sub, sup, tt, var,
+b, u, i, center,
+dl, dt, dd, menu, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, embed,
+figure, figcaption, footer, header, hgroup,
+main, menu, nav, output, ruby, section, summary,
+time, mark, audio, video {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  font-size: 100%;
+  font: inherit;
+  vertical-align: baseline;
+}
+
+/* HTML5 display-role reset for older browsers */
+article, aside, details, figcaption, figure,
+footer, header, hgroup, main, menu, nav, section {
+  display: block;
+}
+/* HTML5 hidden-attribute fix for newer browsers */
+*[hidden] {
+    display: none;
+}
+html{
+	&::-webkit-scrollbar{
+        width: 0.0000000001px;
+	} 
+	&::-webkit-scrollbar-thumb{
+        opacity:0;
+        pointer-events: none;
+        z-index: -99999;
+	}
+	&::-webkit-scrollbar-track{
+        opacity:0;
+        pointer-events:none;
+        z-index : -99999;
+	} 
+}
+menu, ol, ul {
+  list-style: none;
+}
+blockquote, q {
+  quotes: none;
+}
+blockquote:before, blockquote:after,
+q:before, q:after {
+  content: '';
+  content: none;
+}
+table {
+  border-collapse: collapse;
+  border-spacing: 0;
+}
+* {
+  box-sizing: border-box;
+}
+body {
+  font-weight: 300;
+  font-family: 'Source Sans Pro', sans-serif;
+  line-height: 1.2;
+  background:linear-gradient(135deg,#e09,#d0e);
+}
+a {
+  text-decoration:none;
+  color:inherit;
+}
+input{
+  border: none;
+  outline: none;
+}
+
+button{
+  cursor: pointer;
+}
+
+`;
+
 root.render(
   <RecoilRoot>
+    <GlobalStyle />
     <App />
   </RecoilRoot>
 );
