3719595f0abc43926cecf268ad30ee8545f382b1
pooh <cktjdgus45@naver.com>
2022-08-09 15:53:21 +0900

    add layout connecting with framer-motion:layoutId

--- src/App.tsx
+++ src/App.tsx
@@ -13,62 +13,54 @@ background: linear-gradient(135deg, #e09, #d0e);
   align-items: center;
 `;
 
+const Grid = styled(motion.div)`
+  display: grid;
+  width: 50vw;
+  gap: 10px;
+  grid-template-columns: repeat(3,1fr);
+  div:first-child,
+  div:last-child{
+    grid-column: span 2;
+  }
+`
+
 const Box = styled(motion.div)`
-  width: 150px;
   height: 150px;
-  border-radius: 30%;
+  border-radius: 10px;
   background-color: #fff;
   display: flex;
   justify-content: center;
   align-items: center;
   font-size: 20px;
-  position: absolute;
-  top: 100px;
 `
 
-const boxVariants = {
-  initial: (back: boolean) => (
-    {
-      x: back ? -500 : 500,
-      opacity: 0,
-      scale: 0
-    }
-  ),
-  animate: {
-    x: 0,
-    opacity: 1,
-    scale: 1,
-    transition: { duration: 1 }
-  },
-  exit: (back: boolean) => (
-    {
-      x: back ? 500 : -500,
-      opacity: 0,
-      scale: 0,
-      transition: { duration: 1 }
-    }
-  )
-}
-
+const Overlay = styled(motion.div)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width:100%;
+  height:100%;
+  position:absolute;
+`
 
 function App() {
-  const [visible, setVisible] = useState(1);
-  const [back, setBack] = useState(false);
-  const goNext = () => {
-    setBack(false);
-    setVisible(prev => prev === 10 ? 10 : prev + 1)
-  };
-  const goBefore = () => {
-    setBack(true);
-    setVisible(prev => prev === 1 ? 1 : prev - 1)
-  };
+  const [selectedId, setSelectedId] = useState<string | null>(null)
+  const items = ['1', '2', '3', '4'];
   return (
     <Wrapper>
+      <Grid>
+        {items.map(item => (
+          <Box onClick={() => setSelectedId(item)} key={item} layoutId={item}>{item}</Box>
+        ))}
+      </Grid>
       <AnimatePresence>
-        <Box custom={back} variants={boxVariants} initial="initial" animate="animate" exit="exit" key={visible}>{visible}</Box>
+        {
+          selectedId ?
+            <Overlay onClick={() => setSelectedId(null)} initial={{ backgroundColor: 'rgba(0,0,0,0)' }} animate={{ backgroundColor: 'rgba(0,0,0,0.5)' }} exit={{ backgroundColor: 'rgba(0,0,0,0)' }}>
+              <Box layoutId={selectedId} style={{ width: 400, height: 200 }}>{selectedId}</Box>
+            </Overlay> : null
+        }
       </AnimatePresence>
-      <button onClick={goBefore}>before</button>
-      <button onClick={goNext}>next</button>
     </Wrapper>
   )
 
