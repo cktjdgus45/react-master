47b2988572c21ab56b072e2c077acff1bcdee3c3
pooh <cktjdgus45@naver.com>
2022-08-21 14:07:43 +0900

    add close movie detail modal button

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -119,6 +119,22 @@ const BigMovie = styled(motion.div)`
     border-radius: 1%;
 `;
 
+const CloseModal = styled.div`
+    cursor: pointer;
+    margin: 1em;
+    position: absolute;
+    right: 0;
+    top: 0;
+    z-index: 2;
+    svg{
+        background-color: #181818;
+    border-radius: 50%;
+    height: 36px;
+    padding: 8px;
+    width: 36px;
+    }
+`;
+
 const MovieDetail = styled(motion.div)``;
 
 const BigCover = styled.div`
@@ -249,6 +265,7 @@ const Home = () => {
     const [index, setIndex] = useState(0);
     const [leaving, setLeaving] = useState(false);
     const onOverlayClick = () => navigate('/');
+    const onCloseModalClick = () => navigate('/');
     const onDetailClick = (movieId: number) => {
         navigate(`/movies/${movieId}`);
     }
@@ -302,6 +319,12 @@ const Home = () => {
                                         clickedMovie && (
                                             <>
                                                 <BigCover style={{ backgroundImage: `linear-gradient(to top,black,transparent),url(${makeImagePath(clickedMovie.backdrop_path)})` }} />
+                                                <CloseModal onClick={onCloseModalClick}>
+                                                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" data-uia="previewModal-closebtn" role="button" aria-label="close">
+                                                        <path fill-rule="evenodd" clip-rule="evenodd" d="M2.29297 3.70706L10.5859 12L2.29297 20.2928L3.70718 21.7071L12.0001 13.4142L20.293 21.7071L21.7072 20.2928L13.4143 12L21.7072 3.70706L20.293 2.29285L12.0001 10.5857L3.70718 2.29285L2.29297 3.70706Z" fill="currentColor">
+                                                        </path>
+                                                    </svg>
+                                                </CloseModal>
                                                 <BigTitle>{clickedMovie.title}</BigTitle>
                                                 <MainInfo>
                                                     <InfoColumn>
