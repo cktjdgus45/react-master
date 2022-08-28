4355564c498e0b1330fd2fb082ff387561dd5538
pooh <cktjdgus45@naver.com>
2022-08-28 16:06:48 +0900

    add main section at Intro

--- src/Routes/Intro.tsx
+++ src/Routes/Intro.tsx
@@ -8,7 +8,8 @@ const Banner = styled.section`
     justify-content: center;
     align-items: center;
     background-image:linear-gradient(to top,black,transparent),url('https://assets.nflxext.com/ffe/siteui/vlv3/ac824598-245b-4828-b14f-5cff9074f4d0/953c5c8a-e9f2-497c-a447-abdcb437183c/KR-ko-20220822-popsignuptwoweeks-perspective_alpha_website_small.jpg');
-
+    background-size: cover;
+    background-position: center center;
 `
 
 const Start = styled.div`
@@ -21,6 +22,10 @@ const BigTitle = styled.h1`
     font-size: 60px;
     font-weight: 700;
     text-align: center;
+`
+const Title = styled.h1`
+    font-size: 50px;
+    font-weight: 700;
 `
 const MiddleTitle = styled.h3`
     font-size: 25px;
@@ -57,6 +62,50 @@ const Button = styled.button`
     color: ${props => props.theme.white.lighter};
 `
 
+const Main = styled.section`
+    width: 100vw;
+    height: 50vh;
+    padding: 20px 40px;
+    border-bottom: 7px solid ${props => props.theme.black.lighter};
+    border-top: 7px solid ${props => props.theme.black.lighter};
+`;
+const ContentWrapper = styled.div`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+const Content = styled.div`
+    width: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+const ContentBox = styled.div`
+    width: 50%;
+`;
+
+const ContentMedia = styled.div`
+    width: 50%;
+    height: 100%;
+`;
+
+const ContentType = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 100%;
+    img{
+        width: 100%;
+        height: 100%;
+        background-size: cover;
+        background-position: center;
+    }
+`
+
 const Intro = () => {
     return (
         <>
@@ -71,6 +120,66 @@ const Intro = () => {
                     </Form>
                 </Start>
             </Banner>
+            <Main>
+                <ContentWrapper>
+                    <Content>
+                        <ContentBox>
+                            <Title>TV로 즐기세요.</Title>
+                            <MiddleTitle>스마트 TV, PlayStation, Xbox, Chromecast, Apple TV, 블루레이 플레이어 등 다양한 디바이스에서 시청하세요.</MiddleTitle>
+                        </ContentBox>
+                    </Content>
+                    <ContentMedia>
+                        <ContentType>
+                            <video autoPlay loop muted src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/video-tv-0819.m4v"></video>
+                        </ContentType>
+                    </ContentMedia>
+                </ContentWrapper>
+            </Main>
+            <Main>
+                <ContentWrapper>
+                    <ContentMedia>
+                        <ContentType>
+                            <img alt='kids' src='https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/mobile-0819.jpg'></img>
+                        </ContentType>
+                    </ContentMedia>
+                    <Content>
+                        <ContentBox>
+                            <Title>다양한 디바이스에서 시청하세요.</Title>
+                            <MiddleTitle>각종 영화와 시리즈를 스마트폰, 태블릿, 노트북, TV에서 무제한으로 스트리밍하세요. 추가 요금이 전혀 없습니다.</MiddleTitle>
+                        </ContentBox>
+                    </Content>
+                </ContentWrapper>
+            </Main>
+            <Main>
+                <ContentWrapper>
+                    <Content>
+                        <ContentBox>
+                            <Title>즐겨 보는 콘텐츠를 저장해 오프라인으로 시청하세요.</Title>
+                            <MiddleTitle>간편하게 저장하고 빈틈없이 즐겨보세요.</MiddleTitle>
+                        </ContentBox>
+                    </Content>
+                    <ContentMedia>
+                        <ContentType>
+                            <video autoPlay loop muted src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/video-devices.m4v"></video>
+                        </ContentType>
+                    </ContentMedia>
+                </ContentWrapper>
+            </Main>
+            <Main>
+                <ContentWrapper>
+                    <ContentMedia>
+                        <ContentType>
+                            <img alt='kids' src='https://occ-0-395-325.1.nflxso.net/dnm/api/v6/19OhWN2dO19C9txTON9tvTFtefw/AAAABe3Dqef6rg30BxYraI75i97IeQjD0YxUuToAnSA23dl0XQFrjXaFTWIB0HHy4TH_s094NU-9IjLpE_96AvWpTZTAQOR_icyEYcsB.png?r=acf'></img>
+                        </ContentType>
+                    </ContentMedia>
+                    <Content>
+                        <ContentBox>
+                            <Title>어린이 전용 프로필을 만들어 보세요.</Title>
+                            <MiddleTitle>자기만의 공간에서 좋아하는 캐릭터와 즐기는 신나는 모험. 자녀에게 이 특별한 경험을 선물하세요. 넷플릭스 회원이라면 무료입니다.</MiddleTitle>
+                        </ContentBox>
+                    </Content>
+                </ContentWrapper>
+            </Main>
         </>
     )
 }
