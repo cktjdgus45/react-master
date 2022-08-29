18c2c7e5aef30ec38eb23fa9d8867845828ba07a
pooh <cktjdgus45@naver.com>
2022-08-29 23:10:57 +0900

    add social login icon

--- src/Routes/Login.tsx
+++ src/Routes/Login.tsx
@@ -5,6 +5,7 @@ import { useLocation, useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import AuthService from '../firebase/auth_service';
 
+
 const LoginWrapper = styled.section`
     width: 100vw;
     height: 100vh;
@@ -48,6 +49,27 @@ const LoginButton = styled.button`
     font-size: 17px;
     font-weight: 600;
 `;
+
+const SocialButton = styled.button`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    margin-right: 5px;
+    transition: background-color 200ms ease-in-out;
+    &:hover{
+        background-color: ${props => props.theme.black.lighter};
+    }
+`
+const SocialAuthBox = styled.div`
+    width: 100%;
+    height: auto;
+    display: flex;
+    align-items: center;
+    padding: 10px;
+`
 
 interface IFormData {
     email: string;
@@ -96,6 +118,7 @@ const Login = ({ authService }: ILoginProps) => {
         })
     })
 
+
     return (
         <LoginWrapper>
             <Form onSubmit={handleSubmit(onSubmit)}>
@@ -103,8 +126,16 @@ const Login = ({ authService }: ILoginProps) => {
                 <Input type='text' placeholder='이메일 주소 또는 전화번호' defaultValue={email ? email : ''}{...register('email')}></Input>
                 <Input type='password' placeholder='비밀번호' {...register('password')}></Input>
                 <LoginButton>로그인</LoginButton>
-                <button onClick={() => onClick('Google')}>Google</button>
-                <button onClick={() => onClick('Github')}>Github</button>
+                <SocialAuthBox>
+                    <SocialButton onClick={() => onClick('Google')}>
+                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/36px-Google_%22G%22_Logo.svg.png?20210618182606 1.5x, https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/48px-Google_%22G%22_Logo.svg.png?20210618182606 2x" alt="google" />
+                    </SocialButton>
+                    <SocialButton onClick={() => onClick('Github')}>
+                        <svg height="32" aria-hidden="true" viewBox="0 0 16 16" version="1.1" width="32" data-view-component="true">
+                            <path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path>
+                        </svg>
+                    </SocialButton>
+                </SocialAuthBox>
             </Form>
         </LoginWrapper>
     )
